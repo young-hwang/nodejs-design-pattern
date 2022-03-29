dcfecb4d736562749364116aa5c9e90a3f283b49
Young Hwang <ykcul02@gmail.com>
2022-03-29 21:04:23 +0900

    docs :

---
+++ 03.02.05_web_spider_v4/index.js
@@ -0,0 +1,97 @@
+'use strict';
+
+// const request = require('request'); deprecated
+const axios = require('axios')
+const fs = require('fs')
+const mkdirp = require('mkdirp')
+const path = require('path')
+
+const utilities = require('./utilities')
+const TaskQueue = require('./taskQueue')
+
+const downloadQueue = new TaskQueue(2);
+
+const saveFile = (filename, content, callback) => {
+  mkdirp(path.dirname(filename), (err) => {       // 3
+    if (err) {
+      return callback(err)
+    }
+    fs.writeFile(filename, content, callback);   // 4
+  })
+};
+
+const download = (url, filename, callback) => {
+  axios
+    .get(url)
+    .then(res => {                                    // 2
+      const content = res.data;
+      saveFile(filename, content, (err) => {
+        if (err) {
+          callback(err)
+        }
+        console.log(`Downloaded and saved: ${url}`);
+        callback(null, content)
+      })
+    })
+    .catch(err => {
+      callback(err)
+    })
+};
+
+function spiderLinks(currentUrl, body, nesting, callback) {
+  if (nesting === 0) {
+    return process.nextTick(callback)
+  }
+
+  const links = utilities.getPageLinks(currentUrl, body)
+
+  if (links.length === 0) {
+    return process.nextTick(callback)
+  }
+
+  let completed = 0
+  let hasErrors = false
+
+  links.forEach(link => {
+    downloadQueue.push(done => {
+      spider(link, nesting - 1, err => {
+        if (err) {
+          hasErrors = true
+          return callback(err)
+        }
+        if (++completed === links.length && !hasErrors) {
+          callback()
+        }
+        done()
+      })
+    })
+  })
+}
+
+function spider(url, nesting, callback) {
+  const filename = utilities.urlToFilename(url)
+  fs.readFile(filename, 'utf8', (err, body) => {
+    if (err) {
+      if (err.code !== 'ENOENT') {
+        return callback(err)
+      }
+      return download(url, filename, (err, body)  => {
+        if (err) {
+          callback(err)
+        }
+        spiderLinks(url, body, nesting, callback)
+      })
+    }
+
+    spiderLinks(url, body, nesting, callback)
+  })
+}
+
+spider(process.argv[2], 1, (err, filename, downloaded) => {
+  if (err) {
+    console.log(err)
+    process.exit()
+  } else if (downloaded) {
+    console.log(`Download complete`)
+  }
+})
