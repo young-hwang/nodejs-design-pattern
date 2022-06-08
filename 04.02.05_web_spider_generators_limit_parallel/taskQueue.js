c643b81a71da22200282e0915853f88401884396
Young Hwang <ykcul02@gmail.com>
2022-06-08 23:56:04 +0900

    docs :

---
+++ 04.02.05_web_spider_generators_limit_parallel/taskQueue.js
@@ -0,0 +1,29 @@
+'use strict'
+
+class TaskQueue {
+  constructor(concurrency) {
+    this.concurrency = concurrency;
+    this.running = 0;
+    this.taskQueue = [];
+    this.consumerQueue = [];
+    this.spawnWorkers(concurrency);
+  }
+
+  pushTask(task) {
+    this.queue.push(task);
+    this.next();
+  }
+
+  next() {
+    while (this.running < this.concurrency && this.queue.length) {
+      const task = this.queue.shift();
+      task.then(() => {
+        this.running--;
+        this.next();
+      });
+      this.running++
+    }
+  }
+}
+
+module.exports = TaskQueue;
