598318a8010a9e52c5759db59fbcfad38111508f
Young Hwang <ykcul02@gmail.com>
2022-06-13 22:00:12 +0900

    docs :

--- 05.01.02_gzip_streams/index.js
+++ 05.01.02_gzip_streams/index.js
@@ -4,6 +4,6 @@ const zlib = require('zlib');
 const file = process.argv[2];
 
 fs.createReadStream(file)
-.pipe(zlib.createGunzip())
-.pipe(fs.createWriteStream(file + '.gz'))
-.on('finish', () => console.log('File successfully created.'));
+  .pipe(zlib.createGzip())
+  .pipe(fs.createWriteStream(file + '.gz'))
+  .on('finish', () => console.log('File successfully created.'));
