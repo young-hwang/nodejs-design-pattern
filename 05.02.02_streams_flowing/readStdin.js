d17cb882415ec086cd6f0e1ed026e826abed1fd1
Young Hwang <ykcul02@gmail.com>
2022-06-20 23:06:31 +0900

    docs :

---
+++ 05.02.02_streams_flowing/readStdin.js
@@ -0,0 +1,10 @@
+'use restrict';
+
+process.stdin
+  .on('data', (chunk) => {
+    console.log('New data available');
+    console.log(`Chunk read: (${chunk.length}) "${chunk.toString()}"`)
+  })
+  .on('end', () => {
+    process.stdout.write('End of stream');
+  });
