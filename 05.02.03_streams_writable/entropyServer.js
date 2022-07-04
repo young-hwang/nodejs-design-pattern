85f59b6534fd06b98b727eec33ba4be46b941d21
Young Hwang <ykcul02@gmail.com>
2022-07-04 23:34:34 +0900

    docs :

---
+++ 05.02.03_streams_writable/entropyServer.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const Chance = require('chance');
+const chance = new Chance();
+
+require('http').createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  while(chance.bool({likelihood: 95})) {
+    res.write(chance.string() + '\n');
+  }
+  res.end('\nThe end...\n');
+  res.on('finish', () => console.log('All data was sent'));
+}).listen(8080, () => console.log('Listening on http://localhost:8080'));
