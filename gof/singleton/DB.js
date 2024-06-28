7534aee364e5499e4f4a4fc10575d4fdb7ef79dc
young-hwang <ykcul02@gmail.com>
2024-06-29 03:17:05 +0900

    add: singleton pattern

---
+++ gof/singleton/DB.js
@@ -0,0 +1,18 @@
+const URL = 'mongodb://localhost:27017/test';
+const createConnection = url => ({"url": url})
+class DB {
+  constructor(url) {
+    if (!DB.instance) {
+      DB.instance = createConnection(url)
+    }
+    return DB.instance
+  }
+
+  connect() {
+    return this.instance
+  }
+}
+
+const a = new DB(URL);
+const b = new DB(URL)
+console.log(a === b);
