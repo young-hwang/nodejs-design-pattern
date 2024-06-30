b1d1b2d388724df6287fc490e23be8fc08366c42
young-hwang <ykcul02@gmail.com>
2024-06-30 13:11:03 +0900

    add: factory method pattern

---
+++ gof/02_factory_method/coffee.js
@@ -0,0 +1,39 @@
+class Latte {
+  constructor() {
+    this.name = 'latte'
+  }
+}
+
+class Espresso {
+  constructor() {
+    this.name = 'espresso'
+  }
+}
+
+class LatteFactory {
+  static createCoffee() {
+    return new Latte()
+  }
+}
+
+class EspressoFactory {
+  static createCoffee() {
+    return new Espresso()
+  }
+}
+
+const factoryList = { LatteFactory, EspressoFactory }
+
+class CoffeeFactory {
+  static createCoffee(type) {
+    const factory = factoryList[type]
+    return factory.createCoffee()
+  }
+}
+
+const main = () => {
+  const coffee = CoffeeFactory.createCoffee("LatteFactory")
+  console.log(coffee.name)
+}
+
+main()
